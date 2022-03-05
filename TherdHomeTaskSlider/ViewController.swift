347211eef8a1ca5f26239f388e3647719c9642bd
sergeykin <sergeykin1997@mail.ru>
2022-03-05 21:01:48 +0300

    feat: created project

---
+++ TherdHomeTaskSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TherdHomeTaskSlider
+//
+//  Created by Sergey Runovich on 5.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
